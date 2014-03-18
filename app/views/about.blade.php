837e55d08f5166611c4c73c767d03b3b99366e42
kevin <kevin@kevin.local>
2014-03-17 20:21:50 -0700

    moved contact info from about us to contact us page

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -15,18 +15,6 @@
 	<h2></h2>
 
 	<h2>Out promise</h2>
-
-	<h2>Contact Information</h2>
-	<p>Telephone: 82759802375980</p>
-	<p>Fax: numbers: 9083426757896</p>
-	<p>E-mail: info@alnaqaa.com, sales@alnaqaa.com</p>
-	<p>Facebook:</p>
-	<p>Twitter:</p>
-	<p>Google+</p>
-	
-	<p>Physical mail: .............</p>
-	<p>Address: </p>
-	<p>MAP.......</p>
 </div>
 @stop
 @stop
--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.master')
 @section('content')
+<h1 class="text-center">Contact Us</h1>
+<p>Telephone: 82759802375980</p>
+<p>Fax: numbers: 9083426757896</p>
+<p>E-mail: info@alnaqaa.com, sales@alnaqaa.com</p>
+<p>Facebook:</p>
+<p>Twitter:</p>
+<p>Google+</p>
 
+<p>Physical mail: .............</p>
+<p>Address: </p>
+<p>MAP.......</p>
 @stop
