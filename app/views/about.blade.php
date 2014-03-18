57d22e547e8975144a4c645c432634b916d134c1
kevin <kevin@kevin.local>
2014-03-17 18:46:29 -0700

    added about page skeleton

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -1,4 +1,32 @@
 @extends('layouts.master')
 @section('content')
+<div class="container-full" id="about">
+	<h1 class="text-center">About ALNAQAA</h1>
+	<h2>Who are we?</h2>
+	<h3>Our professional staff:</h3>
+	<h3>ALNAQAA Story</h3>
+	<h3>Services Overview</h3>
+	<h3>Mission Statement</h3>
 
+	<h2>CUSTOMERS AND PARTNERS</h2>
+	<h3>TESTIMONIALS</h3>
+
+	<h2>Awards</h2>
+	<h2></h2>
+
+	<h2>Out promise</h2>
+
+	<h2>Contact Information</h2>
+	<p>Telephone: 82759802375980</p>
+	<p>Fax: numbers: 9083426757896</p>
+	<p>E-mail: info@alnaqaa.com, sales@alnaqaa.com</p>
+	<p>Facebook:</p>
+	<p>Twitter:</p>
+	<p>Google+</p>
+	
+	<p>Physical mail: .............</p>
+	<p>Address: </p>
+	<p>MAP.......</p>
+</div>
+@stop
 @stop
